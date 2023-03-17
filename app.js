f25d34d28dad427d0e211256fdb8abf5b4b278a9
AshokkumarAnnadurai <ashokkumar.workofficial@gmail.com>
2023-03-17 13:15:37 +0530

    small change in name

--- app.js
+++ app.js
@@ -18,3 +18,23 @@
 // console.log(newitems);
 // console.log("hello people")
 
+const http = require('http');
+
+const server = http.createServer((req , res)=>{
+    if(req.url === '/'){
+       res.end("home page");
+    }
+    if(req.url === '/about' ){
+        // Blocking Code !!!
+        for (let i = 0 ; i < 100 ; i++){
+            for ( let j = 0; j < 100 ; j++){
+                console.log(`${i} ${j}`);
+            }
+        }
+        res.end('About page')
+    }
+})
+
+server.listen(5000, ()=>{
+    console.log("server listening on ......5000");
+})
